21afcfc58a3fc2295e9146e9fb87a7eeaa36fb74
Khoa Le Rikkei <116610704+khoalyRikkei@users.noreply.github.com>
2023-11-09 13:43:25 +0700

    up new

---
+++ src/messages/index.js
@@ -0,0 +1,25 @@
+export const MSG_COMMON = {
+  MSG_ERROR: {
+    BadRequestException: "Yêu cầu không hợp lệ",
+    UnauthorizedException: "Unauthorized",
+    NotFoundException: "Không tìm thấy tài nguyên",
+    ForbiddenException: "Từ chối truy cập",
+    InternalServerException: "Lỗi server",
+  },
+  MSG_SUCCESS: {
+    create: (data) => `Create ${data} successfully`,
+    read: (data) => `get ${data} successfully`,
+    update: (data) => `Update ${data} successfully`,
+    delete: (data) => `Delete ${data} successfully`,
+  },
+  MSG_FAILURE: {
+    create: (data) => `Create ${data} failure`,
+    read: (data) => `get ${data} failure`,
+    update: (data) => `Update ${data} failure`,
+    delete: (data) => `Delete ${data} failure`,
+  },
+};
+
+export const MSG_VALIDATION = {
+  UnauthorizedException: "Email hoặc mật khẩu không đúng",
+};
---
+++ src/repositories/auth.repository.js
@@ -0,0 +1,44 @@
+import { ServerException } from "../expeiptions/index.js";
+import { MSG_COMMON } from "../messages/index.js";
+import User from "../models/user.model.js";
+const User_2 = new User();
+
+const getData = () => {
+  try {
+    const data = [{ name: "" }];
+
+    return data;
+  } catch (err) {
+    throw new ServerException(MSG_COMMON.MSG_ERROR.InternalServerException);
+  }
+};
+
+const insertUser = async (data) => {
+  try {
+    const user = await User.create({ ...data });
+    return user;
+  } catch (error) {
+    throw new ServerException(MSG_COMMON.MSG_ERROR.InternalServerException, {
+      msg: "DDawng ki khong thanh cong",
+    });
+  }
+};
+
+const getAllUsers = async () => {
+  try {
+    console.log(111);
+    const users = await User.findAll();
+    console.log(users);
+    return users;
+  } catch (error) {
+    throw new ServerException(MSG_COMMON.MSG_ERROR.InternalServerException, {
+      msg: "DDawng ki khong thanh cong",
+    });
+  }
+};
+
+export default {
+  getData,
+  insertUser,
+  getAllUsers,
+};
